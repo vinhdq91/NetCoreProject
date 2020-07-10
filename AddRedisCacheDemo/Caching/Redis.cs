02533e879413cad5d999469c1b611c91b4675050
vinhdq <Administrator@106-53-PC>
2020-07-10 19:23:19 +0700

    Commit redis project

---
+++ AddRedisCacheDemo/Caching/Redis.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddRedisCacheDemo.Caching
+{
+    public class Redis
+    {
+        private readonly IDistributedCache _distributedCache;
+        public Redis(IDistributedCache _distributedCache)
+        {
+            this._distributedCache = _distributedCache;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var cacheKey = "The Time";
+            var currentTime = DateTime.Now.ToString();
+            var cachedTime = _distributedCache.GetString(cacheKey);
+            if (string.IsNullOrEmpty(cachedTime))
+            {
+                // cachedTime = "Expired";
+                // Cache expire trong 5s
+                var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(5));
+                // Nạp lại giá trị mới cho cache
+                _distributedCache.SetString(cacheKey, currentTime, options);
+                cachedTime = _distributedCache.GetString(cacheKey);
+            }
+            var result = $"Current Time : {currentTime} \nCached  Time : {cachedTime}";
+            return result;
+        }
+    }
+}
---
+++ TagHelperExercise/CustomTagHelpers/EmailTags.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperExercise.CustomTagHelpers
+{
+    public class EmailTags : TagHelper
+    {
+        public string Email { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto: {Email}");
+            if (output.Attributes.ContainsName("class"))
+            {
+                var classes = output.Attributes["class"].Value.ToString();
+                output.Attributes.SetAttribute("class", classes += " btn btn-success");
+            }
+            else
+            {
+                output.Attributes.SetAttribute("class", "btn btn-success");
+            }
+        }
+    }
+}
